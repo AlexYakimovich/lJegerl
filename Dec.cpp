f96c3da3356e0d4d0d488df0e5382cbd9a536328
lJegerl <96889152+lJegerl@users.noreply.github.com>
2021-12-30 23:44:25 +0300

    Add files via upload

---
+++ Dec.cpp
@@ -0,0 +1,55 @@
+﻿// Dec.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "deque.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "for int:" << endl;
+	deque <int> a;
+	a.PushLast(1);
+	a.PushLast(5);
+	a.PushLast(8);
+	a.PushLast(0);
+	deque <int> a2 = a;
+	a2.Print(cout);
+	a.dsort();
+	a.Print(cout);
+	a.PrintReverse(cout);
+	cout << "for double:" << endl;
+	deque <double> d;
+	d.PushLast(0.25);
+	d.PushLast(4);
+	d.PushFirst(3.5);
+	d.PushLast(2.0);
+	deque <double> d2 = d;
+	d2.Print(cout);
+	d.dsort();
+	d.Print(cout);
+	d.PrintReverse(cout);
+	cout << "for char:" << endl;
+	deque<const char*> ch;
+	ch.PushLast("I");
+	ch.PushLast("love");
+	ch.PushLast("Anime");
+	ch.PushLast("!");
+	deque <const char*> ch2 = ch;
+	ch.Print(cout);
+	ch.PrintReverse(cout);
+	ch.dsort();
+	ch.Print(cout);
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ deque.h
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+template <class InfoType>
+
+class deque
+{
+private:
+
+    struct QItem
+    {
+        InfoType info;
+        QItem* next;
+        QItem* prev;
+        QItem(InfoType Ainfo) : info(Ainfo), prev(nullptr), next(nullptr) {}
+    };
+
+    QItem* first, * last;
+    unsigned size;
+
+    void Erase();
+    void Clone(const deque& D);
+    InfoType GetElement(int);
+    bool Empty() const;
+
+public:
+
+    const InfoType GetFirst() const;
+    const InfoType GetLast() const;
+    unsigned GetSize();
+
+    deque();
+    deque(const deque&);
+    ~deque();
+
+    friend ostream& operator << (ostream&, const deque<InfoType>&);
+    ostream& Print(ostream&);
+    ostream& PrintReverse(ostream&);
+
+    deque& operator = (const deque&);
+
+
+    void PushLast(InfoType);
+    bool PopLast();
+    void PushFirst(InfoType);
+    bool PopFirst();
+
+    void dsort();
+};
+
+
+
+
+template <class InfoType>
+bool deque <InfoType> ::Empty() const {
+    if (first == nullptr)
+        return true;
+    return false;
+}
+
+template <class InfoType>
+const InfoType deque <InfoType> ::GetFirst() const
+{
+    if (Empty())
+    {
+        throw runtime_error("Пустой дек");
+    }
+
+    return first->info;
+}
+template <class InfoType>
+const InfoType deque <InfoType> ::GetLast() const
+{
+    if (Empty())
+    {
+        throw runtime_error("Пустой дек");
+    }
+
+    return last->info;
+}
+template <class InfoType>
+ostream& operator << (ostream& s, const deque<InfoType>& D)
+{
+
+    for (int i = 0; i < D.size; i++)
+    {
+        s << D.getElement(i) << " ";
+    }
+    s << endl;
+    for (int i = D.size - 1; i >= 0; i--)
+    {
+        s << D.getElement(i) << " ";
+    }
+    s << endl;
+
+    return s;
+}
+
+
+
+template <class InfoType>
+void deque <InfoType> ::Erase() {
+    while (PopLast());
+    size = 0;
+}
+
+template <class InfoType>
+void deque <InfoType> ::Clone(const deque<InfoType>& D) {
+    QItem* tmp = D.last;
+    for (unsigned i = 0; i < D.size; i++)
+    {
+        PushLast(tmp->info);
+        tmp = tmp->next;
+    }
+}
+
+template <class InfoType>
+InfoType deque <InfoType> ::GetElement(int number) {
+    if (!Empty()) {
+        QItem* tmp = last;
+        for (int i = 0; i < number; i++)
+        {
+            tmp = tmp->next;
+        }
+
+        return tmp->info;
+    }
+    else {
+        return NULL;
+    }
+}
+
+
+////////////////////////////////////2
+template <class InfoType>
+deque <InfoType> ::deque() : first(nullptr), last(nullptr), size(0) {
+
+}
+
+template <class InfoType>
+deque<InfoType> ::deque(const deque<InfoType>& D) {
+    size = 0;
+    Clone(D);
+}
+
+template <class InfoType>
+deque <InfoType> :: ~deque() {
+    Erase();
+}
+
+////////////////////////////////////////3
+template <class InfoType>
+void deque <InfoType>::PushLast(InfoType Ainfo) {
+    QItem* tmp = new QItem(Ainfo);
+    if (size > 0)
+    {
+        tmp->prev = first;
+        first->next = tmp;
+    }
+    else
+    {
+        last = tmp;
+    }
+    first = tmp;
+    size++;
+}
+template <class InfoType>
+void deque <InfoType>::PushFirst(InfoType Ainfo) {
+    QItem* tmp = new QItem(Ainfo);
+    if (size > 0)
+    {
+        tmp->next = last;
+        last->prev = tmp;
+    }
+    else
+    {
+        first = tmp;
+    }
+    last = tmp;
+    size++;
+}
+
+template <class InfoType>
+bool deque <InfoType>::PopLast() {
+    if (size == 0) {
+        return false;
+    }
+    QItem* tmp = first;
+    if (size > 1) {
+        first = first->prev;
+    }
+    delete tmp;
+    size--;
+    first->next = nullptr;
+    if (size == 0)
+    {
+        first = nullptr;
+        last = nullptr;
+    }
+    return true;
+}
+
+template <class InfoType>
+bool deque <InfoType>::PopFirst() {
+    if (size == 0) {
+        return false;
+    }
+    QItem* tmp = last;
+    last = last->next;
+    delete tmp;
+    size--;
+    if (last) {
+        last->prev = nullptr;
+    }
+    if (size == 0)
+    {
+        first = nullptr;
+        last = nullptr;
+    }
+    return true;
+}
+template <class InfoType>
+deque<InfoType>& deque <InfoType>::operator = (const deque<InfoType>& D) {
+    if (this != &D)
+    {
+        Erase();
+        Clone(D);
+    }
+    return *this;
+
+}
+
+////////////////////////////////////4
+template <class InfoType>
+unsigned deque <InfoType>::GetSize() {
+    return size;
+}
+
+///////////////////////////////////5
+template<class InfoType>
+ostream& deque<InfoType>::Print(ostream& s) {
+    for (int i = 0; i < size; i++)
+    {
+        s << GetElement(i) << " ";
+    }
+    s << endl;
+    return s;
+}
+template <class InfoType>
+ostream& deque <InfoType> ::PrintReverse(ostream& s) {
+    for (int i = size - 1; i >= 0; i--)
+    {
+        s << GetElement(i) << " ";
+    }
+    s << endl;
+    return s;
+}
+
+///////////////////////////////6
+template <class InfoType>
+void deque <InfoType>::dsort() {
+    vector <InfoType> massive;
+    int dsize = size;
+    for (int i = 0; i < dsize; i++)
+    {
+        massive.push_back(last->info);
+        PopFirst();
+    }
+    sort(massive.rbegin(), massive.rend());
+    for (int i = 0; i < dsize; i++)
+    {
+        InfoType tmp = massive[i];
+        PushFirst(tmp);
+    }
+}
+
